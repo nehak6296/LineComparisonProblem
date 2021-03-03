6d7f62d0b6328b5b9c79156130c67ccb1bbfe388
nehak6296 <nehak6296@gmail.com>
2021-03-03 23:47:05 +0530

    [Neha]Add:Calculating Length of Line

--- Solution3/LineComparisonProblem/Program.cs
+++ Solution3/LineComparisonProblem/Program.cs
@@ -6,7 +6,21 @@ namespace LineComparisonProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Problem");
-        }
+            Console.WriteLine("Enter X1 :");
+            double X1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter X2 :");
+            double X2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter Y1 :");
+            double Y1 = Convert.ToDouble(Console.ReadLine());
+                       
+            Console.WriteLine("Enter Y2 :");
+            double Y2 = Convert.ToDouble(Console.ReadLine());
+
+            double Length = Convert.ToDouble((Math.Sqrt(((X2 - X1) * (X2 - X1)) + ((Y2 - Y1) * (Y2 - Y1)))));
+             
+            Console.WriteLine("Length of Line:" + Length);
+        }                       
     }
 }
